5e72b55072d812db3eec10120c8f695c8f7d3481
“Binod <23531binod.2021@gmail.com>
2024-01-11 23:14:54 +0530

    added

---
+++ WhatsappAutomation.py
@@ -0,0 +1,3 @@
+import pywhatkit
+pywhatkit.sendwhatmsg("+917325062518","just sleep now",23,1)   # receiver mob no with country code,message,hour,minutes
+# In 51 seconds web.whatsapp.com will open and after 20 seconds message will be delivered
